3cb7e95c94de1d27612a5034b43a040fa7cae882
Santiago Madariaga <santiago.madariaga.contractor@bbva.com>
2023-07-09 21:36:15 -0500

    Adding files

---
+++ build/CMakeFiles/testa.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/testa.dir/main.cpp.obj"
+  "CMakeFiles/testa.dir/main.cpp.obj.d"
+  "CMakeFiles/testa.dir/pizza.cpp.obj"
+  "CMakeFiles/testa.dir/pizza.cpp.obj.d"
+  "libtesta.dll.a"
+  "testa.exe"
+  "testa.exe.manifest"
+  "testa.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/testa.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
