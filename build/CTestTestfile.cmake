8f2df6d1a45ec7f35f23aec2a0d441b06d7ff450
Santiago Madariaga <santiago.madariaga.contractor@bbva.com>
2023-07-09 23:33:52 -0500

    eliminating build

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/olapu/Documents/Temp/Tarea2
-# Build directory: C:/Users/olapu/Documents/Temp/Tarea2/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
